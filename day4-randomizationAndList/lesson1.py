6cecd7dce7de86761f582b9180f393f7d3dc566c
jkhan2211 <junaidkhandev123@gmail.com>
2022-08-01 15:59:03 -0400

    added labs and lessons

---
+++ day4-randomizationAndList/lesson1.py
@@ -0,0 +1,12 @@
+import random
+# import my_module
+
+random_integer = random.randint(1, 10)
+
+print(random_integer)
+
+# print(my_module.pi)
+
+#0.00000 - 0.99999999
+random_float = random.random() * 5
+print(random_float)
