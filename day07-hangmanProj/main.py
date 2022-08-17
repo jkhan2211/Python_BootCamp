6d251464612249de9a01bc52b7c25061606a6db4
jkhan2211 <junaidkhandev123@gmail.com>
2022-08-17 10:49:20 -0400

    code complete

--- day07-hangmanProj/main.py
+++ day07-hangmanProj/main.py
@@ -1,16 +1,104 @@
-#Step 1 
-import random
-word_list = ["aardvark", "baboon", "camel"]
+#Step 4
 
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-chosen_word =random.choice(word_list)
-print(chosen_word)
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("Guess a letter: ")
-guess = guess.lower()
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-for letter in chosen_word:
-    if letter == guess:
-        print("You guessed a correct letter!")
-    else:
-        print("You guessed a wrong letter!")
+import random
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+#TODO-1: - Create a variable called 'lives' to keep track of the number of lives left. 
+#Set 'lives' to equal 6.
+lives = 6
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+
+#Create blanks
+display = []
+for _ in range(word_length):
+    display += "_"
+end_of_game = False
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+    # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+    
+     #TODO-2: - If guess is not a letter in the chosen_word,
+    #Then reduce 'lives' by 1. 
+    #If lives goes down to 0 then the game should stop and it should print "You lose."
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            print("You lose.")
+            end_of_game = True
+            break
+     #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    if "_" not in display:
+        print("You win!")
+        end_of_game = True
+    
+    #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
+    print(stages[lives])
