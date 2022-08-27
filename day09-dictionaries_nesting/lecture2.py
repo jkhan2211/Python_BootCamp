c4417f999e6866ee4ef9045851bcce6f07283853
jkhan2211 <junaidkhandev123@gmail.com>
2022-08-27 00:25:17 -0400

    completed

---
+++ day09-dictionaries_nesting/lecture2.py
@@ -0,0 +1,35 @@
+#Nesting
+
+capitals = {
+
+    "France": "Paris",
+    "Germany": "Berlin"
+}
+
+# Nesting a List in a dictionary
+
+travel_log = {
+
+    "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
+    "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttguart"], "total_visits": 5}
+}
+
+#Nesting Dictionary in a Dictionary 
+travel_log = {
+
+    "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
+    "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttguart"], "total_visits": 5}
+}
+
+
+#Nesting Dictionary in a List
+travel_log = [
+    {"country": "France", 
+    "cities_visited": ["Paris", "Lille", "Dijon"], 
+    "total_visits": 12
+    },
+    {"country": "Germany", 
+    "cities_visited": ["Berlin", "Hamburg", "Stuttguart"], 
+    "total_visits": 5
+    }
+]
