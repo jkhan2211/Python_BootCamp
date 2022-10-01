eb68d1fdb19b7a55df14ec1d1bc5d3f9b126f56c
jkhan2211 <junaidkhandev123@gmail.com>
2022-09-30 20:25:03 -0400

    docstring doc

--- day10-function-with-outputs/calculator/main.py
+++ day10-function-with-outputs/calculator/main.py
@@ -29,24 +29,27 @@ operations = {
     "/": divide
 }
 
-num1 = int(input("What is the first number: "))
+def calculator():
+    num1 = int(input("What is the first number: "))
 
+    for symbol in operations:
+        print(symbol)
 
-for symbol in operations:
-    print(symbol)
+    should_continue = True
 
-should_continue = True
+    while should_continue:
 
-while should_continue:
+        operation_symbol = input("Pick an operation:  ")
+        num2 = int(input("What is the next number: "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
 
-    operation_symbol = input("Pick an operation:  ")
-    num2 = int(input("What is the next number: "))
-    calculation_function = operations[operation_symbol]
-    answer = calculation_function(num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-    print(f"{num1} {operation_symbol} {num2} = {answer}")
+        if input(f"Type 'y' to continue calculating with {answer} or type 'n' to exit: ") == "y":
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
 
-    if input(f"Type 'y' to continue calculating with {answer} or type 'n' to exit: ") == "y":
-        num1 = answer
-    else:
-        should_continue = False
+calculator()
