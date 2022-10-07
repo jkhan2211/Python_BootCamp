7f79263d8281fb27437244d1c46e0efa1a91893b
jkhan2211 <junaidkhandev123@gmail.com>
2022-10-06 22:30:44 -0400

    added

---
+++ day12-scope/lesson1.py
@@ -0,0 +1,17 @@
+################### Scope ####################
+
+enemies = 1
+
+def increase_enemies():
+  enemies = 2
+  print(f"enemies inside function: {enemies}")
+
+increase_enemies()
+print(f"enemies outside function: {enemies}")
+
+# Local Scope
+def drink_potion():
+    potion_strength = 2
+    print(f"potion strength: {potion_strength}")
+
+drink_potion()
